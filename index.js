2b0afda4d6d63d5ab98b9a26982f922aede6da7d
Joseph Swetnam <jbswetnam@gmail.com>
2015-04-16 21:28:55 -0400

    complete refactoring to use more directives than jquery selectors

---
+++ index.js
@@ -0,0 +1,39 @@
+angular.module('homePage', [])
+  .directive('fginderNavbar', function() {
+    return {
+      link: function(scope, element) {
+        element.width($(window).width() - 20);
+      }
+    };
+  })
+  .directive('fginderPhotoContainer', function() {
+    return {
+      link: function(scope, element) {
+        element.height($(window).height() - 100);
+      }
+    };
+  })
+  .directive('fginderFullsize', function() {
+    return {
+      link: function(scope, element) {
+        element.css('max-width', $(window).width());
+      }
+    };
+  })
+  .directive('fginderImageCover', function() {
+    return {
+      link: function(scope, element) {
+        element[0].oncontextmenu = function(e) {
+          e.preventDefault();
+        }
+      }
+    };
+  })
+  .controller('photoController', ['$scope', function($scope) {
+    $scope.photos = [
+      'black_white_street',
+      'black_white_alley',
+      'brick_warehouse',
+      'graffiti_shed'
+    ];
+  }]);
