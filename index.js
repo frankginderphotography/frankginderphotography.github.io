6bbfe6eeb0a4e4bdfa784f19a9360567aa7b14bb
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-18 09:53:22 -0400

    deleted photos directory

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+njn.controller('sidebar', {
+  groups: [
+    '1-12', '13-24', '25-36', '37-48', '49-60', '61-72', '73-84', '85-96', '97-108', '109-120', '121-123'
+  ]
+});
+
 var photos = [];
 var indexRange = (location.hash.match(/#\/([0-9]+-[0-9]+)/) || ['','0-19'])[1];
 document.getElementById('leave-showcase').href = '#/' + indexRange;
@@ -11,7 +17,11 @@ for(var i = firstIndex; i <= lastIndex; i++) {
   photos.push({ src: src, id: id, href: href });
 }
 
-njn.controller('photos', {  photos: photos });
+njn.controller('photo-gallery', {  photos: photos });
+
+[].forEach.call(document.getElementsByClassName('thumbnail'), function(thumbnail) {
+  if(thumbnail.height > thumbnail.width) thumbnail.className = 'thumbnail-portrait';
+});
 
 var showcase = document.getElementById('showcase');
 var leftClick = document.getElementById('left-click');
@@ -19,18 +29,32 @@ var rightClick = document.getElementById('right-click');
 
 var resizeImg = function(img) {
   if(img.clientWidth) {
-    if(window.innerHeight > window.innerWidth) {
-      img.width = window.innerWidth - 204;
-      img.removeAttribute('height');
-      img.style.top = (window.innerHeight - img.height) / 2 + 'px';
-    } else {
-      img.height = window.innerHeight;
-      img.style.top = '0px';
-      img.removeAttribute('width');
-    }
+    (function detectWidthHeight(portrait) {
+      if(portrait) {
+        img.width = window.innerWidth - 104;
+        if(img.height < window.innerHeight) {
+          img.removeAttribute('height');
+          img.style.top = (window.innerHeight - img.height) / 2 + 'px';
+        } else {
+          detectWidthHeight(false);
+        }
+      } else {
+        img.height = window.innerHeight;
+        if(img.width < window.innerWidth - 103) {
+          img.style.top = '0px';
+          img.removeAttribute('width');
+        } else {
+          detectWidthHeight(true);
+        }
+      }
+    })(window.innerHeight > window.innerWidth);
     img.style.left = Math.round((window.innerWidth - img.width) / 2) + 'px';
+    leftClick.style.top = img.style.top;
+    leftClick.style.height = img.height - 2 + 'px';
     leftClick.style.left = parseInt(img.style.left) - 52 + 'px';
     rightClick.style.left = parseInt(img.style.left) + img.width + 'px';
+    rightClick.style.top = img.style.top;
+    rightClick.style.height = img.height - 2 + 'px';
   } else {
     window.setTimeout(resizeImg.bind(null, img), 500);
   }
@@ -39,26 +63,31 @@ var resizeImg = function(img) {
 var photoGridSquare;
 
 (window.onhashchange = function() {
-  var prevImg = showcase.lastChild;
-  var photoId = location.hash.match(/photo_[0-9]+$/);
-  if(photoGridSquare) photoGridSquare.className = 'photo-grid-square';
-  if(photoId) {
-    var img = document.getElementById(photoId[0]);
-    photoGridSquare = img.parentElement.parentElement.parentElement.parentElement;
-    photoGridSquare.className = 'photo-grid-square-behind';
-    var clone = img.cloneNode();
-    clone.id = '';
-    prevImg.tagName === 'IMG' ? showcase.replaceChild(clone, prevImg) : showcase.appendChild(clone);
-    clone.className = 'fullsize';
-    showcase.style.display = 'block';
-    resizeImg(clone);
-    var photoNumber = +photoId[0].match(/[0-9]+/)[0];
-    var prevIndex = photoNumber > firstIndex ? photoNumber - 1 : lastIndex;
-    leftClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + prevIndex;
-    var nextIndex = photoNumber < lastIndex ? photoNumber + 1 : firstIndex;
-    rightClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + nextIndex;
+  var newRange = location.hash.match(/#\/([0-9]+-[0-9]+)/);
+  if(newRange && newRange[1] !== indexRange) {
+    location.reload(true);
   } else {
-    showcase.style.display = 'none';
+    var prevImg = showcase.lastChild;
+    var photoId = location.hash.match(/photo_[0-9]+$/);
+    if(photoGridSquare) photoGridSquare.className = 'photo-grid-square';
+    if(photoId) {
+      var img = document.getElementById(photoId[0]);
+      photoGridSquare = img.parentElement.parentElement.parentElement.parentElement;
+      photoGridSquare.className = 'photo-grid-square-behind';
+      var clone = img.cloneNode();
+      clone.id = '';
+      prevImg.tagName === 'IMG' ? showcase.replaceChild(clone, prevImg) : showcase.appendChild(clone);
+      clone.className = 'fullsize';
+      showcase.style.display = 'block';
+      resizeImg(clone);
+      var photoNumber = +photoId[0].match(/[0-9]+/)[0];
+      var prevIndex = photoNumber > firstIndex ? photoNumber - 1 : lastIndex;
+      leftClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + prevIndex;
+      var nextIndex = photoNumber < lastIndex ? photoNumber + 1 : firstIndex;
+      rightClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + nextIndex;
+    } else {
+      showcase.style.display = 'none';
+    }
   }
 })();
 
