3a89d6509c7e0e705e879e4a50af1d3e57605f2d
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-19 19:30:05 -0400

    refactor html & css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // break up into groups of twelve, returning strings like: '1-12', '31-42', etc:
 
 var groups = [], max = 151;
+
 for(var i = 1, j = 1; i <= max; i++) {
   if(i - j === 11 || i === max) {
     groups.push(j + '-' + i);
@@ -13,15 +14,12 @@ njn.controller('sidebar', { groups: groups });
 
 var photos = [];
 var indexRange = (location.hash.match(/#\/([0-9]+-[0-9]+)/) || ['','1-12'])[1];
-
-var showcase = document.getElementById('showcase');
-var photoFloater = document.getElementById('photo-floater');
-var fullsizeA = document.getElementById('fullsize-a');
-var leftClick = document.getElementById('left-click');
-var rightClick = document.getElementById('right-click');
+var showcase = document.getElementsByClassName('showcase')[0];
+var photoFloater = document.getElementsByClassName('photo-floater')[0];
+var fullsizeA = document.getElementsByClassName('fullsize-a')[0];
 
 fullsizeA.href = '#/' + indexRange;
-document.getElementById('leave-showcase').children[0].href = '#/' + indexRange;
+document.getElementsByClassName('leave-showcase')[0].children[0].href = '#/' + indexRange;
 
 var firstIndex = +indexRange.split('-')[0];
 var lastIndex = +indexRange.split('-')[1];
@@ -31,36 +29,29 @@ for(var i = firstIndex; i <= lastIndex; i++) {
   var src = 'photos/' + id + '.jpg';
   var href = '#/' + indexRange + '/' + id;
   var thumbnail = 'thumbnails/' + id + '.png';
-  photos.push({ src: src, id: id, href: href, thumbnail: thumbnail });
+  photos.push({ src: src, id: id, href: href, thumbnail: thumbnail, photoInd: i - firstIndex });
 }
 
 njn.controller('photo-gallery', {  photos: photos });
 
-var resizeImg = function(img) {
-  if(img.clientWidth) {
-    (function detectWidthHeight(portrait) {
-      if(portrait) {
-        img.width = window.innerWidth - 104;
-        img.removeAttribute('height');
-        if(img.height < window.innerHeight) {
-          img.style.top = (window.innerHeight - img.height) / 2 + 'px';
-        } else {
-          detectWidthHeight(false);
-        }
-      } else {
-        img.height = window.innerHeight;
-        img.removeAttribute('width');
-        if(img.width < window.innerWidth - 103) {
-          img.style.top = '0px';
-        } else {
-          detectWidthHeight(true);
-        }
-      }
-    })(window.innerHeight > window.innerWidth);
-    photoFloater.style.width = img.width + 105 + 'px';
-    photoFloater.style.height = img.height - 2 + 'px';
-  } else {
-    window.setTimeout(resizeImg.bind(null, img), 500);
+var loaded = {};
+
+function loadFullSizeImg(photo, i) {
+  var fullsize = loaded[photo.src];
+  if(!fullsize) {
+    fullsize = new Image();
+    fullsize.src = photo.src;
+    fullsize.className = 'fullsize';
+    loaded[photo.src] = fullsize;
+  }
+  if(!i) {
+    var prevImg = showcase.getElementsByClassName('fullsize')[0];
+    fullsizeA.replaceChild(fullsize, prevImg);
+    showcase.style.display = 'block';
+  } else if(i === 1) {
+    document.getElementById('right-click').href = photo.href;
+  } else if(i === -1) {
+    document.getElementById('left-click').href = photo.href;
   }
 }
 
@@ -75,29 +66,18 @@ var photoGridSquare;
     var photoId = location.hash.match(/photo_[0-9]+$/);
     if(photoGridSquare) photoGridSquare.className = 'photo-grid-square';
     if(photoId) {
-      var img = document.getElementById(photoId[0]);
-      photoGridSquare = img.parentElement.parentElement;
-      photoGridSquare.className = 'photo-grid-square-behind';
-      var clone = img.cloneNode();
-      clone.id = '';
-      fullsizeA.replaceChild(clone, prevImg);
-      clone.className = 'fullsize';
-      showcase.style.display = 'block';
-      resizeImg(clone);
-      var photoNumber = +photoId[0].match(/[0-9]+/)[0];
-      var prevIndex = photoNumber > firstIndex ? photoNumber - 1 : lastIndex;
-      leftClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + prevIndex;
-      var nextIndex = photoNumber < lastIndex ? photoNumber + 1 : firstIndex;
-      rightClick.getElementsByTagName('a')[0].href = '#/' + indexRange + '/photo_' + nextIndex;
+      photoGridSquare = document.querySelector('.photo-grid-square:hover');
+      if(photoGridSquare) photoGridSquare.className = 'photo-grid-square-behind';
+      var thumbnail = document.getElementById(photoId);
+      var photoIndex = +photoId[0].match(/[0-9]+/)[0];
+      var startIndex = +thumbnail.dataset.photoindex;
+      for(var i = 0, diffs = [0, 1, -1, 2, -2]; i < 5; i++) {
+        var currIndex = (startIndex + diffs[i]) % photos.length;
+        var currPhoto = photos.slice(currIndex)[0];
+        loadFullSizeImg(currPhoto, diffs[i]);
+      }
     } else {
       showcase.style.display = 'none';
     }
   }
-})();
-
-(window.onresize = function() {
-  var img = photoFloater.children[1].children[0];
-  if(img) resizeImg(img);
-  showcase.style.width = window.innerWidth + 'px';
-  showcase.style.height = window.innerHeight + 'px';
 })();
