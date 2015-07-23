64a99b04f0d4a697991acc0d5b73ab867604b7c8
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-23 14:35:04 -0400

    alert firstTouch

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ showcases.addEventListener('touchstart', function(e) {
 }, false);
 
 showcases.addEventListener('touchmove', function(e) {
-alert(firstTouch.pageY, firstTouch.pageX);
+alert(firstTouch.pageY + ' ' + firstTouch.pageX);
   var currTouch = e.touches[0];
   // Ensure this is a one touch swipe and not, e.g. a pinch:
   if (currTouch.length > 1 || (e.scale && e.scale !== 1)) {
