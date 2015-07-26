edeaf6f87869ff62f993e939c639481b5b889ee8
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-25 22:16:53 -0400

    testing swipe

--- index.js
+++ index.js
@@ -205,6 +205,6 @@ showcases.addEventListener('touchmove', function(e) {
   } else {
     // disable horizontal scrolling:
     e.preventDefault();
-    currentlyShown.style.transform = 'translateX(' + deltaX + 'px)';
+    currentlyShown.style.transform = '-webkit-translateX(' + deltaX + 'px)';
   }
 }, false);
