6619255632f47d4e0fd2d8e890bd5f8652e1dae9
Joseph Swetnam <jbswetnam@gmail.com>
2015-07-28 09:05:48 -0400

    looking for ios issue

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ function loadShowcase(photoId) {
       showcase.className = 'showcase';
     }
     if(!i) shownShowcase = showcase;
+    console.log(i);
     currentlyShown.appendChild(showcase);
   }
   loadAhead(startIndex);
